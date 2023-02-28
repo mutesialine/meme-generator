73fc9941565d8a47be1591206043606b79b77ad3
Mutesi <ktesialine@gmail.com>
2023-02-28 15:29:22 +0200

    fix:add alt on image tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ export default function Header(){
   return (
     <div className="bg-purple text-white py-5 px-4 flex justify-between">
       <div className="flex gap-x-2 items-center">
-        <img src="/Troll Face.png" className="w-8" />
+        <img src="/Troll Face.png" className="w-8" alt="face image"/>
         <p className="text-xl font-bold">Meme generator</p>
       </div>
 
